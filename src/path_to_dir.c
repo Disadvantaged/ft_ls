0e52f7c81e95aa4c8d995fc1367afb00fc830310
Dimitris GOLEAR <dgolear@e1p39.chisinau.42.fr>
2017-01-27 15:39:23 +0200

    changed structure of file path_to_dir

--- src/path_to_dir.c
+++ src/path_to_dir.c
@@ -6,23 +6,57 @@
 /*   By: dgolear <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/27 12:14:41 by dgolear           #+#    #+#             */
-/*   Updated: 2017/01/27 14:23:10 by dgolear          ###   ########.fr       */
+/*   Updated: 2017/01/27 15:38:17 by dgolear          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	path_to_dir(t_option *options, t_list **dir, t_list **file)
+static t_list	*create_dir(t_option *options, int pos)
 {
-	int			i;
-	struct stat	statbuf;
 	DIR			*directory;
 	t_directory	*dirt;
-	t_file		*filet;
 	t_list		*node;
 
-	if (dir || file)
-		;
+	if ((dirt = (t_directory*)malloc(sizeof(t_directory))) == NULL
+		|| (node = (t_list *)malloc(sizeof(t_list))) == NULL
+		|| (directory = opendir(options->paths[pos])) == NULL)
+	{
+		ft_printf("ft_ls: %s: %s\n", options->paths[pos], strerror(errno));
+		exit(errno);
+	}
+	dirt->path = ft_strdup(options->paths[pos]);
+	dirt->dir = directory;
+	node->content = dirt;
+	node->next = NULL;
+	node->content_size = sizeof(dirt);
+	return (node);
+}
+
+static	t_list	*create_file(t_option *options, int pos, struct stat statbuf)
+{
+	t_file	*filet;
+	t_list	*node;
+
+	if ((filet = (t_file*)malloc(sizeof(t_file))) == NULL
+		|| (node = (t_list *)malloc(sizeof(t_list))) == NULL)
+	{
+		ft_printf("ft_ls: %s: %s\n", options->paths[pos], strerror(errno));
+		exit(errno);
+	}
+	filet->path = ft_strdup(options->paths[pos]);
+	filet->file = statbuf;
+	node->content = filet;
+	node->next = NULL;
+	node->content_size = sizeof(filet);
+	return (node);
+}
+
+void			path_to_dir(t_option *options, t_list **dir, t_list **file)
+{
+	struct stat	statbuf;
+	int			i;
+
 	i = 0;
 	while (i < options->cursize)
 	{
@@ -30,29 +64,9 @@ void	path_to_dir(t_option *options, t_list **dir, t_list **file)
 			exit(ft_printf("ft_ls: %s: %s\n",
 						options->paths[i], strerror(errno)));
 		if (statbuf.st_mode & S_IFDIR && !options->flags[9].sign)
-		{
-			if ((directory = opendir(options->paths[i])) == NULL)
-				exit(ft_printf("ft_ls: %s: %s\n",
-						options->paths[i], strerror(errno)));
-			if ((dirt = (t_directory*)malloc(sizeof(t_directory))) == NULL)
-				exit(ft_printf("ft_ls: %s", strerror(errno)));
-			node = ft_lstnew(&dirt, sizeof(dirt));
-			((t_directory *)node->content)->path = ft_strdup(options->paths[i]);
-			((t_directory *)node->content)->dir = directory;
-			ft_lstadd(dir, node);
-			
-		}
+			ft_lstadd(dir, create_dir(options, i));
 		else
-		{
-			if ((filet = (t_file*)malloc(sizeof(t_file))) == NULL
-			|| (node = (t_list *)malloc(sizeof(t_list))) == NULL)
-				exit(ft_printf("ft_ls: %s", strerror(errno)));
-			filet->path = ft_strdup(options->paths[i]);
-			filet->file = statbuf;
-			node->content = filet;
-			node->next = NULL;
-			ft_lstadd(file, node);
-		}
+			ft_lstadd(file, create_file(options, i, statbuf));
 		i++;
 	}
 }
