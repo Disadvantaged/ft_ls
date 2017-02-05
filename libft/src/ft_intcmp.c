c5ceb2f42b43b1d356e166d28f94d88bc6d7de6e
Dimitris GOLEAR <dgolear@e1p67.chisinau.42.fr>
2017-02-05 13:07:25 +0200

    added list sorting algorithm

--- libft/src/ft_intcmp.c
+++ libft/src/ft_intcmp.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_intcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dgolear <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/01/18 15:25:36 by dgolear           #+#    #+#             */
-/*   Updated: 2017/02/05 13:06:17 by dgolear          ###   ########.fr       */
+/*   Created: 2017/02/05 12:44:03 by dgolear           #+#    #+#             */
+/*   Updated: 2017/02/05 12:45:26 by dgolear          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_ls.h"
+#include "libft.h"
 
-void	sort_files_and_dirs(t_options *options, t_list *dir, t_list *file)
+int		ft_intcmp(const void *a, const void *b)
 {
-	if (options->flags[6])
-		return ;
-
-}
-
-int		main(int ac, char **av)
-{
-	t_option		*options;
-	t_list			*dir;
-	t_list			*file;
-
-	options = check_options(ac, av);
-	dir = NULL;
-	file = NULL;
-	path_to_dir(options, &dir, &file);
-	sort_files_and_dirs(options, &dir, &file);
-	return (0);
+	return (*(int *)a - *(int *)b);
 }
